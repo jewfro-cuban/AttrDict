eaf8753d354ebd863557f0c8eaa72f8e9d08ca98
jewfro-cuban <jewfrocuban1@gmail.com>
2019-08-18 11:01:46 +0300

    expose ACAttrDict

--- attrdict/__init__.py
+++ attrdict/__init__.py
@@ -3,8 +3,8 @@ attrdict contains several mapping objects that allow access to their
 keys as attributes.
 """
 from attrdict.mapping import AttrMap
-from attrdict.dictionary import AttrDict
+from attrdict.dictionary import AttrDict, ACAttrDict
 from attrdict.default import AttrDefault
 
 
-__all__ = ['AttrMap', 'AttrDict', 'AttrDefault']
+__all__ = ['AttrMap', 'AttrDict', 'ACAttrDict', 'AttrDefault']
